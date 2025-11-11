b9f2e5c0620dc06095e24bd423c91391fe6b91ee
김우승 <161958686+victory0319@users.noreply.github.com>
2025-11-12 00:31:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def formula(molecule):
 
 st.title("화학식량 계산기")
 
-molecule = st.text_input("분자식 입력", "")
+molecule = st.text_input("분자식 입력", "H2O")
 mass = st.number_input("분자의 질량 입력 (g)", value=0)
 
 if molecule:
